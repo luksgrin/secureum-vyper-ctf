99f45282d50875932524d143ba9a0301545b9148
luksgrin <lugoibel@doctor.upv.es>
2024-02-25 14:15:53 +0000

    Fixed WETH issue: etched bytecode into test

--- challenges/test/federicobianucci-SNAKE-presale/SNAKEPresaleTest.t.sol
+++ challenges/test/federicobianucci-SNAKE-presale/SNAKEPresaleTest.t.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.13;
 import {Test, console2} from "forge-std/Test.sol";
 import {SNAKEPresaleDeployer} from "script/federicobianucci-SNAKE-presale/SNAKEPresaleDeployer.s.sol";
 import {IPresale} from "src/federicobianucci-SNAKE-presale/interfaces/IPresale.sol";
+import {IWETH} from "src/federicobianucci-SNAKE-presale/interfaces/IWETH.sol";
 
 /*///////////////////////////////////////////////
 // Import dependencies for your solution here! //
@@ -14,14 +15,26 @@ import {IPresale} from "src/federicobianucci-SNAKE-presale/interfaces/IPresale.s
 /// CTFTest to showcase the solution to the CTF
 contract CTFTest is Test, SNAKEPresaleDeployer {
     IPresale public snakePresale;
+    IWETH public weth9 = IWETH(0x6B5817E7091BC0C747741E96820b0199388245EA);
 
     /// @notice Deploy the CTF and the solution contract
     function setUp() public override(SNAKEPresaleDeployer) {
+        etch_weth9_code();
         SNAKEPresaleDeployer.setUp();
 
         snakePresale = IPresale(deploySNAKEPresaleCTF());
 
         vm.deal(address(snakePresale), 100 ether);
+    }
+
+    /// @notice Etch weth9 bytes into its address for local testing
+    function etch_weth9_code() private {
+        vm.etch(
+            address(weth9),
+            vm.parseBytes(vm.readFile(
+                "src/federicobianucci-SNAKE-presale/WETH9.bin"
+            ))
+        );
     }
 
     /// @notice Test that the CTF is unsolved if we don't do anything
